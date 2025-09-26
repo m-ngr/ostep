ab1271e18cfa1e305ee94fce99c062191b23f4c0
Mahmoud Elnagar <mahmoud.elnagar1698@gmail.com>
2025-09-27 01:22:25 +0300

    loop app

---
+++ projects/scheduling-xv6-lottery/src/loop.c
@@ -0,0 +1,22 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "pstat.h"
+
+int main(int argc, char *argv[]) {
+  int n = 1;
+  int pid = getpid();
+
+  if (argc > 1) n = atoi(argv[1]);
+
+  if (settickets(n) != 0){
+    printf(1, "ERROR: settickets failed\n");
+    exit();
+  }
+
+  printf(1, "[%d] started with %d tickets\n", pid, n);
+
+  while(1) {}
+
+  exit();
+}
